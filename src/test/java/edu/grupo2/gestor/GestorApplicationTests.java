3beefddd9a1dbdc68741d8a37630d8a7f0ea6bb7
jpdpalacios <jpdpalacios.dev@gmail.com>
2023-09-14 10:00:58 -0300

    añade la estructura inicial del proyecto

---
+++ src/test/java/edu/grupo2/gestor/GestorApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.grupo2.gestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
