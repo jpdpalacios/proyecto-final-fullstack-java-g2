f81a2ad01e04e923bd456ff0f8a47cf4805ec31f
Matías Gabriel Patzer <matias.patzer.work@gmail.com>
2023-09-18 16:07:18 -0300

    Agregadas funcionalidades de las ventanas

--- src/main/java/edu/gestor/mgp/controller/ClienteController.java
+++ src/main/java/edu/gestor/mgp/controller/ClienteController.java
@@ -54,4 +54,5 @@ public class ClienteController {
         return "redirect:/clientes/listar";
     }
 
+
 }
--- src/main/java/edu/gestor/mgp/controller/UsuarioController.java
+++ src/main/java/edu/gestor/mgp/controller/UsuarioController.java
@@ -25,3 +25,6 @@ public class UsuarioController {
         return "lista_usuarios";
     }
 }
+
+
+
