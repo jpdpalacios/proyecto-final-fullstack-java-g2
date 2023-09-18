61bee49a3668a7c657973409f494cca7d670f896
jpdpalacios <jpdpalacios.dev@gmail.com>
2023-09-18 15:39:10 -0300

    metodo POST de trabajo a realizar

--- src/main/java/edu/gestor/mgp/controller/TrabajoARealizarController.java
+++ src/main/java/edu/gestor/mgp/controller/TrabajoARealizarController.java
@@ -1,11 +1,17 @@
 package edu.gestor.mgp.controller;
 
+import edu.gestor.mgp.model.Admin;
+import edu.gestor.mgp.model.Cliente;
 import edu.gestor.mgp.model.TrabajoARealizar;
+import edu.gestor.mgp.repository.AdminRepository;
+import edu.gestor.mgp.repository.ClienteRepository;
 import edu.gestor.mgp.repository.TrabajoARealizarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -17,6 +23,12 @@ public class TrabajoARealizarController {
     @Autowired
     private TrabajoARealizarRepository trabajoRepository;
 
+    @Autowired
+    private ClienteRepository clienteRepository; // Repositorio de clientes
+
+    @Autowired
+    private AdminRepository usuarioAdminRepository; // Repositorio de usuarios admin
+
     @GetMapping("/listar")
     public String listarTrabajos(Model model) {
         List<TrabajoARealizar> trabajos = trabajoRepository.findAll();
@@ -24,4 +36,23 @@ public class TrabajoARealizarController {
         return "trabajos";
     }
 
+    @GetMapping("/crear")
+    public String mostrarFormularioCreacion(Model model) {
+        TrabajoARealizar nuevoTrabajo = new TrabajoARealizar();
+        model.addAttribute("nuevoTrabajo", nuevoTrabajo);
+
+        List<Cliente> clientes = clienteRepository.findAll();
+        model.addAttribute("clientes", clientes);
+
+        List<Admin> usuariosAdmin = usuarioAdminRepository.findAll();
+        model.addAttribute("usuariosAdmin", usuariosAdmin);
+
+        return "crear-trabajo";
+    }
+    @PostMapping("/crear")
+    public String crearTrabajo(@ModelAttribute("nuevoTrabajo") TrabajoARealizar nuevoTrabajo) {
+        trabajoRepository.save(nuevoTrabajo);
+
+        return "redirect:/trabajos/listar";
+    }
 }
